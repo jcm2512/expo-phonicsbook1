29e4c613e284f3dd5b3b50e7621cf74eea5b32e1
jcm2512 <joe.magalotti@gmail.com>
2022-12-25 12:28:39 +0900

    fonts, splash-screen

--- App.js
+++ App.js
@@ -5,24 +5,64 @@ import { Button } from "@rneui/base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LinearGradient } from "expo-linear-gradient";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 function HomeScreen({ navigation }) {
+  SplashScreen.preventAutoHideAsync();
+
+  const [appIsReady, setAppIsReady] = React.useState(false);
+
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts
+        await Font.loadAsync({
+          "Lexend-Medium": require("./assets/fonts/Lexend-Medium.ttf"),
+          "Lexend-Black": require("./assets/fonts/Lexend-Black.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <LinearGradient
-        // Background Linear Gradient
         colors={["#FDE633", "#FDBD2C"]}
         style={styles.background}
       />
       <View style={[styles.autowidth, styles.header]}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.headerTextTitle}>English World</Text>
+        <Text style={styles.headerTextSubtitle}>Online</Text>
       </View>
       <View style={[styles.autowidth, styles.content]}>
         <Button
           title="Phonics Book 1"
           buttonStyle={{ borderRadius: 50 }}
           onPress={() => navigation.navigate("Phonics 1")}
-        />{" "}
+        />
       </View>
       <View style={[styles.autowidth, styles.footer]}></View>
 
@@ -44,16 +84,28 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Phonics 1" component={Phonics_1} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Phonics 1" component={Phonics_1} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  headerTextTitle: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 48,
+    textAlign: "center",
+  },
+  headerTextSubtitle: {
+    fontFamily: "Lexend-Black",
+    fontSize: 36,
+    textAlign: "center",
+  },
   background: {
     position: "absolute",
     width: "100%",
